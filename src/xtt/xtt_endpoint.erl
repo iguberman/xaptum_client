85fc4b9b8a9bf92098f48852eb558e0769ae71f7
Irina Guberman <irina.guberman@xaptum.com>
2018-05-14 16:29:28 -0500

    generate credentials on the fly in CT

--- src/xtt/xtt_endpoint.erl
+++ src/xtt/xtt_endpoint.erl
@@ -106,7 +106,6 @@ init_tpm_creds(BaseDir, GroupDir, CertDir, CredDir, TpmHost, TpmPort, TpmPasswor
 init_file_creds(GroupDir, CertDir, CredDir)->
   lager:info("Initializing file creds from GroupDir ~p~n: ~p, ~nCertDir ~p~n:~p, and ~nCredDir ~p~n",
     [GroupDir, os:cmd("ls " ++ GroupDir), CertDir, os:cmd("ls " ++ CertDir), CredDir, os:cmd("ls " ++ CredDir)]),
-  os:cmd()
   #file_creds{
     basename = filename:join([GroupDir, ?BASENAME_FILE]),
     gpk = filename:join([GroupDir, ?DAA_GPK_FILE]),
