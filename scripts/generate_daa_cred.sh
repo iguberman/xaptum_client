f8f42dec336d28abcd757e15446cc3bfc8d32507
Irina Guberman <irina.guberman@xaptum.com>
2018-05-14 13:50:21 -0500

    refactor cred generation scripts to take basedir as optional arg

--- scripts/generate_daa_cred.sh
+++ scripts/generate_daa_cred.sh
@@ -1,32 +1,58 @@
 #!/usr/bin/env bash
 set -x
 
-ID=$1
-
-## This script relies on following ecdaa executables in your path:
-
+## This script generates credentials for a single endpoint
+## It can be used on its own, or run as dependency of generate_multi_daa_creds.sh script
+## which takes care of generating group stuff (common for multiple endpoints)
+#  It relies on the following:
+#  1. ecdaa executables are in your path:
 ##      ecdaa_member_request_join
 ##      ecdaa_issuer_respond_to_join_request
 
+## 2. GROUP_DIR is already populated with isk.bin adn gpk.bin (most likely but not necessarily
+#     by generate_multi_daa_creds.sh scripts)
+##  If they are not, this script should simply be run as dependency of generate_multi_daa_creds.sh
+##  which takes care of generating group stuff.
+
+## 3. The following files are populated
+##    (this would be always true if run as dependency of generate_multi_daa_creds.sh):
+##    $BASEDIR/GROUP/message.bin"
+##    $BASEDIR/GROUP/basename.bin"
+##    $BASEDIR/GROUP/sk_revocation_list.bin"
+##    $BASEDIR/GROUP/bsn_revocation_list.bin"
+
+
+ID=$1
+BASEDIR=${2:-`pwd`}
+
+MEMBER_DIR="$BASEDIR/MEMBER$ID"
+
+GROUP_DIR="$BASEDIR/GROUP"
+
+mkdir -p $CRED_DIR
+
+mkdir -p $GROUP_DIR
+
+
 NONCE="nonce-$ID"
-PK_BIN="MEMBER/pk$ID.bin"
-SK_BIN="MEMBER/sk$ID.bin"
+PK_BIN="$MEMBER_DIR/pk.bin"
+SK_BIN="$MEMBER_DIR/sk.bin"
 
 ecdaa_member_request_join $NONCE $PK_BIN $SK_BIN || exit 1
 
-CRED_BIN="MEMBER/cred$1.bin"
-CRED_SIG_BIN="MEMBER/cred_sig$1.bin"
+CRED_BIN="$MEMBER_DIR/cred.bin"
+CRED_SIG_BIN="$MEMBER_DIR/cred_sig.bin"
 ISK_BIN="GROUP/isk.bin"
 GPK_BIN="GROUP/gpk.bin"
 
 ## GENERATE CREDENTIAL
 ecdaa_issuer_respond_to_join_request $PK_BIN $ISK_BIN $CRED_BIN $CRED_SIG_BIN $NONCE || exit 1
 
-SIG_BIN="MEMBER/sig$1.bin"
-MSG_BIN="MEMBER/message.bin"
-BASENAME_BIN="GROUP/basename.bin"
-SK_REV_LIST_BIN="MEMBER/sk_revocation_list.bin"
-BSN_REV_LIST_BIN="GROUP/bsn_revocation_list.bin"
+SIG_BIN="$MEMBER_DIR/sig.bin"
+MSG_BIN="$GROUP_DIR/message.bin"
+BASENAME_BIN="$GROUP_DIR/basename.bin"
+SK_REV_LIST_BIN="$GROUP_DIR/sk_revocation_list.bin"
+BSN_REV_LIST_BIN="$GROUP_DIR/bsn_revocation_list.bin"
 
 ## SIGN AND VERIFY
 ecdaa_member_sign $SK_BIN $CRED_BIN $SIG_BIN $MSG_BIN $BASENAME_BIN || exit 1
--- scripts/generate_multi_daa_creds.sh
+++ scripts/generate_multi_daa_creds.sh
@@ -3,6 +3,10 @@
 CREDS_RANGE_START=$1
 CREDS_RANGE_END=$2
 
+BASEDIR=${3:-`pwd`}
+GROUP_DIR = "$BASEDIR/GROUP"
+mkdir -p $GROUP_DIR
+
 ## This script relies on following ecdaa executables in your path:
 
 ##      OPTIONAL:
@@ -11,13 +15,14 @@ CREDS_RANGE_END=$2
 ##      ecdaa_issuer_create_group
 ##      ecdaa_extract_group_public_key
 
-GPK_BIN="GROUP/gpk.bin"
-ISK_BIN="GROUP/isk.bin"
+
+GPK_BIN="$GROUP_DIR/gpk.bin"
+ISK_BIN="$GROUP_DIR/isk.bin"
 
 if [[ -f $ISK_BIN && -f $GPK_BIN ]]; then
     echo "File $ISK_BIN and $GPK_BIN already exist"
 else
-    IPK_BIN="GROUP/ipk.bin"
+    IPK_BIN="$GROUP_DIR/ipk.bin"
     ## Create ISSUER public and private key
     ecdaa_issuer_create_group $IPK_BIN $ISK_BIN || exit 1
     ## Extract group public key
@@ -26,9 +31,32 @@ else
     echo "Successfully generated $ISK_BIN and $GPK_BIN"
 fi
 
+MSG_BIN="$GROUP_DIR/message.bin"
+BASENAME_BIN="$GROUP_DIR/basename.bin"
+SK_REV_LIST_BIN="$GROUP_DIR/sk_revocation_list.bin"
+BSN_REV_LIST_BIN="$GROUP_DIR/bsn_revocation_list.bin"
+
+## Copy default files to requested BASEDIR if it doesn't have them already
+
+if [ ! -f $MSG_BIN ]; then
+    cp DEFAULTS/message.bin $MSG_BIN
+fi
+
+if [ ! -f $BASENAME_BIN ]; then
+    cp DEFAULTS/basename.bin $BASENAME_BIN
+fi
+
+if [ ! -f $SK_REV_LIST_BIN ]; then
+    cp DEFAULTS/sk_revocation_list.bin $SK_REV_LIST_BIN
+fi
+
+if [ ! -f $BSN_REV_LIST_BIN ]; then
+    cp DEFAULTS/bsn_revocation_list.bin $BSN_REV_LIST_BIN
+fi
+
 
 for i in $(seq $CREDS_RANGE_START $CREDS_RANGE_END);
 do echo "Generating cred $i"
-./generate_daa_cred.sh $i || exit 1
+./generate_daa_cred.sh $i $BASEDIR || exit 1
 done
 
